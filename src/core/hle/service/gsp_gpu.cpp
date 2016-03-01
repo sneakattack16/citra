bf2b02696f630456cf0be72264a733c150cf718d
mailwl <mailwl@gmail.com>
2016-03-01 19:32:03 +0300

    gsp

--- src/core/hle/service/gsp_gpu.cpp
+++ src/core/hle/service/gsp_gpu.cpp
@@ -285,6 +285,18 @@ static void FlushDataCache(Service::Interface* self) {
               address, size, process);
 }
 
+static void StoreDataCache(Service::Interface* self) {
+    u32* cmd_buff = Kernel::GetCommandBuffer();
+    u32 address = cmd_buff[1];
+    u32 size = cmd_buff[2];
+    u32 process = cmd_buff[4];
+
+    cmd_buff[1] = RESULT_SUCCESS.raw; // No error
+
+    LOG_DEBUG(Service_GSP, "(STUBBED) called address=0x%08X, size=0x%08X, process=0x%08X",
+        address, size, process);
+}
+
 /**
  * GSP_GPU::RegisterInterruptRelayQueue service function
  *  Inputs:
@@ -606,7 +618,7 @@ const Interface::FunctionInfo FunctionTable[] = {
     {0x001C0040, nullptr,                       "SetLedForceOff"},
     {0x001D0040, nullptr,                       "SetTestCommand"},
     {0x001E0080, nullptr,                       "SetInternalPriorities"},
-    {0x001F0082, nullptr,                       "StoreDataCache"},
+    {0x001F0082, StoreDataCache,                "StoreDataCache"},
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
