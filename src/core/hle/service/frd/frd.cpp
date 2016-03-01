82d3ad12f8ac1ccda3ec7f98ca9255b1dfabd7c1
mailwl <mailwl@gmail.com>
2016-03-01 20:41:40 +0300

    frd:u: Initial stub some functions

--- src/core/hle/service/frd/frd.cpp
+++ src/core/hle/service/frd/frd.cpp
@@ -2,6 +2,8 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
+#include "common/string_util.h"
+
 #include "core/hle/service/service.h"
 #include "core/hle/service/frd/frd.h"
 #include "core/hle/service/frd/frd_a.h"
@@ -9,6 +11,69 @@
 
 namespace Service {
 namespace FRD {
+
+void GetMyPresence(Service::Interface* self) {
+    u32* cmd_buff = Kernel::GetCommandBuffer();
+
+    cmd_buff[1] = RESULT_SUCCESS.raw; // No error
+    cmd_buff[2] = 0; // private mode
+    cmd_buff[3] = 0; // don't show current game
+    cmd_buff[4] = 0; // unknown
+
+    LOG_WARNING(Service_FRD, "(STUBBED) called");
+}
+
+void GetFriendKeyList(Service::Interface* self) {
+    u32* cmd_buff = Kernel::GetCommandBuffer();
+
+    u32 unknown   = cmd_buff[1];
+    u32 frd_count = cmd_buff[2];
+    u32 buf_addr  = cmd_buff[65];
+    FriendKey* frd_key_list = (FriendKey*)Memory::GetPointer(buf_addr);
+    memset(frd_key_list, 0, frd_count * sizeof(FriendKey));
+
+    cmd_buff[1] = RESULT_SUCCESS.raw; // No error
+    cmd_buff[2] = 0; // 0 friends
+    LOG_WARNING(Service_FRD, "(STUBBED) called, unknown=%d, frd_count=%d, buf_addr=0x%08X", unknown,
+                frd_count, buf_addr);
+}
+
+void GetFriendProfile(Service::Interface* self) {
+    u32* cmd_buff = Kernel::GetCommandBuffer();
+
+    cmd_buff[1] = RESULT_SUCCESS.raw; // No error
+    LOG_WARNING(Service_FRD, "(STUBBED) called");
+}
+
+void GetFriendScreenName(Service::Interface* self) {
+    u32* cmd_buff = Kernel::GetCommandBuffer();
+
+    cmd_buff[1] = RESULT_SUCCESS.raw; // No error
+    LOG_WARNING(Service_FRD, "(STUBBED) called");
+}
+
+void GetFriendAttributeFlags(Service::Interface* self) {
+    u32* cmd_buff = Kernel::GetCommandBuffer();
+
+    cmd_buff[1] = RESULT_SUCCESS.raw; // No error
+    LOG_WARNING(Service_FRD, "(STUBBED) called");
+}
+
+void GetMyFriendKey(Service::Interface* self) {
+    u32* cmd_buff = Kernel::GetCommandBuffer();
+
+    cmd_buff[1] = RESULT_SUCCESS.raw; // No error
+    LOG_WARNING(Service_FRD, "(STUBBED) called");
+}
+
+void GetMyScreenName(Service::Interface* self) {
+    u32* cmd_buff = Kernel::GetCommandBuffer();
+
+    cmd_buff[1] = 0; // No error
+    // TODO: (mailwl) get the name from config
+    Common::UTF8ToUTF16("Citra").copy((char16_t*)&cmd_buff[2], 20);
+    LOG_WARNING(Service_FRD, "(STUBBED) called");
+}
 
 void Init() {
     using namespace Kernel;
--- src/core/hle/service/frd/frd.h
+++ src/core/hle/service/frd/frd.h
@@ -4,8 +4,74 @@
 
 #pragma once
 
+#include "core/hle/service/service.h"
+
 namespace Service {
 namespace FRD {
+
+struct FriendKey {
+    u32 friend_id;
+    u32 unknown;
+    u64 friend_code;
+};
+
+/**
+ * FRD::GetMyPresence service function
+ *  Outputs:
+ *      1 : Result of function, 0 on success, otherwise error code
+ *      2 : Public mode (0 = private, non-zero = public)
+ *      3 : Show current game (byte, 0 = don't show, non-zero = show)
+ *      4 : Unknown
+ */
+void GetMyPresence(Service::Interface* self);
+
+/**
+ * FRD::GetFriendKeyList service function
+ *  Inputs:
+ *      1 : Unknown
+ *      2 : Max friends count
+ *      65 : Address of FriendKey List
+ *  Outputs:
+ *      1 : Result of function, 0 on success, otherwise error code
+ *      2 : FriendKey count filled
+ */
+void GetFriendKeyList(Service::Interface* self);
+
+/**
+ * FRD::GetFriendProfile service function
+ *  Outputs:
+ *      1 : Result of function, 0 on success, otherwise error code
+ */
+void GetFriendProfile(Service::Interface* self);
+
+/**
+ * FRD::GetFriendAttributeFlags service function
+ *  Outputs:
+ *      1 : Result of function, 0 on success, otherwise error code
+ */
+void GetFriendAttributeFlags(Service::Interface* self);
+
+/**
+ * FRD::GetFriendScreenName service function
+ *  Outputs:
+ *      1 : Result of function, 0 on success, otherwise error code
+ */
+void GetFriendScreenName(Service::Interface* self);
+
+/**
+ * FRD::GetMyFriendKey service function
+ *  Outputs:
+ *      1 : Result of function, 0 on success, otherwise error code
+ */
+void GetMyFriendKey(Service::Interface* self);
+
+/**
+ * FRD::GetMyScreenName service function
+ *  Outputs:
+ *      1 : Result of function, 0 on success, otherwise error code
+ *      2 : UTF16 encoded name
+ */
+void GetMyScreenName(Service::Interface* self);
 
 /// Initialize FRD service(s)
 void Init();
