e7c35f3907d08d0317f089dd844537189361f8ab
MerryMage <MerryMage@users.noreply.github.com>
2016-04-25 08:54:57 +0100

    DSP/HLE: Implement Source processing

--- src/audio_core/hle/dsp.cpp
+++ src/audio_core/hle/dsp.cpp
@@ -2,8 +2,11 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
+#include <array>
+
 #include "audio_core/hle/dsp.h"
 #include "audio_core/hle/pipe.h"
+#include "audio_core/hle/source.h"
 
 namespace DSP {
 namespace HLE {
@@ -34,14 +37,34 @@ static SharedMemory& WriteRegion() {
     return g_regions[CurrentRegion() == 0 ? 1 : 0];
 }
 
+std::array<Source, num_sources> sources = {
+    Source(0), Source(1), Source(2), Source(3), Source(4), Source(5),
+    Source(6), Source(7), Source(8), Source(9), Source(10), Source(11),
+    Source(12), Source(13), Source(14), Source(15), Source(16), Source(17),
+    Source(18), Source(19), Source(20), Source(21), Source(22), Source(23)
+};
+
 void Init() {
     DSP::HLE::ResetPipes();
+    for (auto& source : sources) {
+        source.Reset();
+    }
 }
 
 void Shutdown() {
 }
 
 bool Tick() {
+    SharedMemory& read = ReadRegion();
+    SharedMemory& write = WriteRegion();
+
+    for (size_t i = 0; i < num_sources; i++) {
+        write.source_statuses.status[i] = sources[i].Tick(read.source_configurations.config[i], read.adpcm_coefficients.coeff[i]);
+        for (size_t mix = 0; mix < 3; mix++) {
+            //source[i].MixInto(/*...*/, mix);
+        }
+    }
+
     return true;
 }
 
