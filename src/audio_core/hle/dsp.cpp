ac6d10e7dfc191339bc94949af48844ef1c9cdbd
MerryMage <MerryMage@users.noreply.github.com>
2016-04-25 10:01:37 +0100

    AudioCore: Turn g_region0 and g_region1 into an array g_regions with two elements

--- src/audio_core/hle/dsp.cpp
+++ src/audio_core/hle/dsp.cpp
@@ -8,8 +8,31 @@
 namespace DSP {
 namespace HLE {
 
-SharedMemory g_region0;
-SharedMemory g_region1;
+std::array<SharedMemory, 2> g_regions;
+
+static size_t CurrentRegion() {
+    // The region with the higher frame counter is chosen unless there is wraparound.
+
+    if (g_regions[0].frame_counter == 0xFFFFu && g_regions[1].frame_counter != 0xFFFEu) {
+        // Wraparound has occured.
+        return 1;
+    }
+
+    if (g_regions[1].frame_counter == 0xFFFFu && g_regions[0].frame_counter != 0xFFFEu) {
+        // Wraparound has occured.
+        return 0;
+    }
+
+    return (g_regions[0].frame_counter > g_regions[1].frame_counter) ? 0 : 1;
+}
+
+static SharedMemory& ReadRegion() {
+    return g_regions[CurrentRegion() == 0 ? 0 : 1];
+}
+
+static SharedMemory& WriteRegion() {
+    return g_regions[CurrentRegion() == 0 ? 1 : 0];
+}
 
 void Init() {
     DSP::HLE::ResetPipes();
@@ -20,22 +43,6 @@ void Shutdown() {
 
 bool Tick() {
     return true;
-}
-
-SharedMemory& CurrentRegion() {
-    // The region with the higher frame counter is chosen unless there is wraparound.
-
-    if (g_region0.frame_counter == 0xFFFFu && g_region1.frame_counter != 0xFFFEu) {
-        // Wraparound has occured.
-        return g_region1;
-    }
-
-    if (g_region1.frame_counter == 0xFFFFu && g_region0.frame_counter != 0xFFFEu) {
-        // Wraparound has occured.
-        return g_region0;
-    }
-
-    return (g_region0.frame_counter > g_region1.frame_counter) ? g_region0 : g_region1;
 }
 
 } // namespace HLE
